75a5e55b7ecac5d9b20f3934cd7d2c7f37ea1305
MHWillard <mhwillard@gmail.com>
2024-01-09 15:44:05 -0500

    changed assemblyinfo issues; working on web publish for AWS

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,3 +1,18 @@
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
 namespace Tests
 {
     public class UnitTest1

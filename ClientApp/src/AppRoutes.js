4c25a32a32844171df7f2fceac2c9295869cf792
MHWillard <mhwillard@gmail.com>
2024-01-15 15:22:15 -0500

    building components and functions for first int test pass

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import Feed from './components/Feed.js';
 
 const AppRoutes = [
   {
@@ -14,6 +15,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+    },
+  {
+    path: '/feed',
+    element: <Feed />
   }
 ];
 
---
+++ ClientApp/src/components/Feed.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+import DocumentTitle from "./DocumentTitle.js"
+
+export default function Feed() {
+    DocumentTitle("Sepia Journal - testinguser's Journal")
+    return (
+        <div id="journal-feed">
+            <p>Feed</p>
+        </div>
+    )
+}

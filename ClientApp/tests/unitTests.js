2e1c70899cd6411423c8f401b7e82e8e075e3e63
MHWillard <mhwillard@gmail.com>
2024-02-23 15:56:04 -0500

    add more tests

---
+++ ClientApp/tests/unitTests.js
@@ -0,0 +1,12 @@
+﻿/*
+test: handle component making and passing in login token and testing for render
+
+set login package token
+render component with login information
+
+test for:
+title = has username of user
+new post = option shows up if token is present
+
+ANOTHER TEST: render and test for when these things aren't present
+*/

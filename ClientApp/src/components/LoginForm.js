edb2aa56a1d1c2ae7f830facef6ee00496f2f6a0
MHWillard <mhwillard@gmail.com>
2024-03-18 16:24:29 -0400

    daily

--- ClientApp/src/components/LoginForm.js
+++ ClientApp/src/components/LoginForm.js
@@ -1,27 +1,35 @@
-﻿import { Routes, Route, NavLink, Navigate, useNavigate, } from 'react-router-dom';
+﻿import { Routes, Route, NavLink, Navigate, useNavigate, Redirect } from 'react-router-dom';
 import React, { useState, useEffect, useContext } from 'react';
 
 export default function LoginForm()
 {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    function handleSubmit(event) {
+    const loginDataObject = { username: '', password: '' };
+    const [loginData, setLoginData] = useState(loginDataObject);
+    const navigate = useNavigate();
+
+    function handleChange(event) {
         //get username
         //build feed URL with username
         //redirect
         event.preventDefault();
-        const username = event.target.username-textbox.value;
-        const url = "https://localhost:44440/feed" + username;
-        <Navigate to={url} replace />
+        const { name, value } = event.target;
+        setLoginData(prevState => ({ ...prevState, [name]: value }));
     };
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const username = loginDataObject.username;
+        const url = "https://localhost:44440/feed" + username;
+        return navigate(url);
+    }
 
     return (
         <div>
             <form onSubmit={handleSubmit} >
                 <label htmlFor="login-username-textbox">Username:</label><br></br>
-                <input type="text" id="login-username-textbox" name="username-textbox"></input><br></br>
+                <input type="text" id="login-username-textbox" name="username" value={loginData.username} onChange={handleChange}></input><br></br>
                 <label htmlFor="login-password-textbox">Password:</label><br></br>
-                <input type="text" id="login-password-textbox" name="password-textbox"></input><br></br>
+                <input type="text" id="login-password-textbox" name="password" value={loginData.password} onChange={handleChange}></input><br></br>
                 <button type="submit" id="login-button">Submit</button>
             </form>
         </div>
